f79c3142d0d504022f471b777a11dca05d067b46
Shane Lucy <shanelucy01@protonmail.com>
2021-04-06 18:41:28 +0100

    Created error component

---
+++ src/components/Error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p class="text-red-600">{{ message }}</p>
+    <img
+      v-if="status === 404"
+      src="../assets/404.svg"
+      alt="Octopus attacking scuba diver"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Error",
+  props: {
+    message: String,
+    status: Number,
+  },
+});
+</script>
