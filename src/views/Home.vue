e14c9060ebf00a3d7d5e619b50d09ea619660bc4
Shane Lucy <shanelucy01@protonmail.com>
2021-04-06 18:47:12 +0100

    Added Table to homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="mr-20 ml-20">
+    <Table />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Table from "@/components/Table.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld,
-  },
-
-  mounted: async function () {
-    try {
-      const i = await this.axios.get("ping");
-      console.log(i);
-    } catch (e) {
-      console.log(e.message);
-    }
+    Table,
   },
 });
 </script>
