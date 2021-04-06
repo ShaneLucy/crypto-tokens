6809dffa64bc62d9c8b8dbc8f446bc885e68c3d6
Shane Lucy <shanelucy01@protonmail.com>
2021-04-06 21:32:01 +0100

    Adjusted axios config to work in coverage action

--- tests/unit/axios.spec.ts
+++ tests/unit/axios.spec.ts
@@ -1,5 +1,7 @@
+import axios from "../../src/http";
+
 describe("Axios", () => {
   it("is configured with correct base url", () => {
-    expect(process.env.VUE_APP_BASE_URL).toMatch("https://api.coingecko.com/api/v3/");
+    expect(axios.defaults.baseURL).toMatch("https://api.coingecko.com/api/v3/");
   });
 });
