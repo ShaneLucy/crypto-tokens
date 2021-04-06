d87724e471fc50f469b4ba5316bfed22671d8ef0
Shane Lucy <shanelucy01@protonmail.com>
2021-04-06 18:06:48 +0100

    Created 404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="mr-20 ml-20">
+    <h1 class="text-red-600 text-5xl">Not Found!</h1>
+    <img src="../assets/404.svg" alt="Octopus attacking scuba diver" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
